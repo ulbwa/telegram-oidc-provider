f3061a13e28937c56de745e135dec3c35855cbff
ulbwa <ulbwa@icloud.com>
2026-02-09 18:59:15 +0500

    Добавить реализацию use cases для CreateClient, LoginByTelegramWidget и LoginByTelegramMiniApp

--- internal/infrastructure/di/services.go
+++ internal/infrastructure/di/services.go
@@ -1,9 +1,12 @@
 package di
 
 import (
+	hydra_client "github.com/ory/hydra-client-go"
 	"github.com/samber/do/v2"
 	app_services "github.com/ulbwa/telegram-oidc-provider/internal/application/services"
+	"github.com/ulbwa/telegram-oidc-provider/internal/common"
 	domain_services "github.com/ulbwa/telegram-oidc-provider/internal/domain/services"
+	"github.com/ulbwa/telegram-oidc-provider/internal/infrastructure/hydra"
 	infra_services "github.com/ulbwa/telegram-oidc-provider/internal/infrastructure/services"
 	"github.com/ulbwa/telegram-oidc-provider/internal/infrastructure/storage/postgres"
 	"gorm.io/gorm"
@@ -15,12 +18,44 @@ func provideServices(injector do.Injector) {
 		return infra_services.NewUUIDv7Generator()
 	})
 
-	// Application services
+	// Application services - Database
 	do.Provide(injector, func(i do.Injector) (app_services.Transactor, error) {
 		db, err := do.Invoke[*gorm.DB](i)
 		if err != nil {
 			return nil, err
 		}
 		return postgres.NewTransactor(db), nil
+	})
+
+	// Application services - Hydra
+	do.Provide(injector, func(i do.Injector) (*hydra_client.APIClient, error) {
+		cfg, err := do.Invoke[*common.Config](i)
+		if err != nil {
+			return nil, err
+		}
+
+		configuration := hydra_client.NewConfiguration()
+		configuration.Servers = []hydra_client.ServerConfiguration{
+			{
+				URL: cfg.Hydra.AdminURL,
+			},
+		}
+		return hydra_client.NewAPIClient(configuration), nil
+	})
+
+	do.Provide(injector, func(i do.Injector) (app_services.HydraLoginManager, error) {
+		client, err := do.Invoke[*hydra_client.APIClient](i)
+		if err != nil {
+			return nil, err
+		}
+		return hydra.NewHydraClient(client)
+	})
+
+	do.Provide(injector, func(i do.Injector) (app_services.HydraClientManager, error) {
+		client, err := do.Invoke[*hydra_client.APIClient](i)
+		if err != nil {
+			return nil, err
+		}
+		return hydra.NewHydraClient(client)
 	})
 }
--- internal/infrastructure/di/usecases.go
+++ internal/infrastructure/di/usecases.go
@@ -1,7 +1,112 @@
 package di
 
-import "github.com/samber/do/v2"
+import (
+	"time"
+
+	"github.com/samber/do/v2"
+	app_services "github.com/ulbwa/telegram-oidc-provider/internal/application/services"
+	"github.com/ulbwa/telegram-oidc-provider/internal/application/usecases"
+	"github.com/ulbwa/telegram-oidc-provider/internal/domain/repositories"
+)
 
 func provideUsecases(injector do.Injector) {
-	// TODO: provide use cases here
+	// CreateClient use case
+	do.Provide(injector, func(i do.Injector) (*usecases.CreateClient, error) {
+		hydraClient, err := do.Invoke[app_services.HydraClientManager](i)
+		if err != nil {
+			return nil, err
+		}
+		transactor, err := do.Invoke[app_services.Transactor](i)
+		if err != nil {
+			return nil, err
+		}
+		botRepo, err := do.Invoke[repositories.BotRepository](i)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: Implement TelegramBotVerifier
+		return usecases.NewCreateClient(
+			nil, // botVerifier - TODO
+			hydraClient,
+			transactor,
+			botRepo,
+		), nil
+	})
+
+	// LoginByTelegramWidget use case
+	do.Provide(injector, func(i do.Injector) (*usecases.LoginByTelegramWidget, error) {
+		userRepo, err := do.Invoke[repositories.UserRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		loginRepo, err := do.Invoke[repositories.UserBotLoginRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		botRepo, err := do.Invoke[repositories.BotRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		hydra, err := do.Invoke[app_services.HydraLoginManager](i)
+		if err != nil {
+			return nil, err
+		}
+		transactor, err := do.Invoke[app_services.Transactor](i)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: Implement Telegram services
+		return usecases.NewLoginByTelegramWidget(
+			time.Hour*24,
+			userRepo,
+			loginRepo,
+			botRepo,
+			hydra,
+			transactor,
+			nil, // parser - TODO
+			nil, // verifier - TODO
+			nil, // replayGuard - TODO
+			nil, // userFactory - TODO
+		), nil
+	})
+
+	// LoginByTelegramMiniApp use case
+	do.Provide(injector, func(i do.Injector) (*usecases.LoginByTelegramMiniApp, error) {
+		userRepo, err := do.Invoke[repositories.UserRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		loginRepo, err := do.Invoke[repositories.UserBotLoginRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		botRepo, err := do.Invoke[repositories.BotRepository](i)
+		if err != nil {
+			return nil, err
+		}
+		hydra, err := do.Invoke[app_services.HydraLoginManager](i)
+		if err != nil {
+			return nil, err
+		}
+		transactor, err := do.Invoke[app_services.Transactor](i)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: Implement Telegram services
+		return usecases.NewLoginByTelegramMiniApp(
+			time.Hour*24,
+			userRepo,
+			loginRepo,
+			botRepo,
+			hydra,
+			transactor,
+			nil, // parser - TODO
+			nil, // verifier - TODO
+			nil, // replayGuard - TODO
+			nil, // userFactory - TODO
+		), nil
+	})
 }
