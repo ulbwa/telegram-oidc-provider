77450df789ba767f6796036d88e45c9afb67cdec
ulbwa <ulbwa@icloud.com>
2026-02-25 20:49:15 +0500

    chore: add utilities and Docker Compose configuration

--- internal/transport/http/public/handlers/users/get_user.go
+++
@@ -1,7 +0,0 @@
-package users
-
-import "github.com/gofiber/fiber/v2"
-
-func (c *UsersController) GetUser(ctx *fiber.Ctx) error {
-	return nil
-}
--- internal/transport/http/public/handlers/users/list_users.go
+++
@@ -1,7 +0,0 @@
-package users
-
-import "github.com/gofiber/fiber/v2"
-
-func (c *UsersController) ListUsers(ctx *fiber.Ctx) error {
-	return nil
-}
