4e7733979a1258ff16b2c86c6344085f25774f6e
ulbwa <ulbwa@icloud.com>
2026-02-28 20:28:45 +0500

    refactor: simplify login use case error handling and improve input structure

--- internal/interface/http/web/login.go
+++ internal/interface/http/web/login.go
@@ -5,51 +5,31 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog"
 	"github.com/ulbwa/telegram-oidc-provider/internal/application/usecase"
 )
 
 func (s *server) Login(c echo.Context) error {
-	output, err := s.resolveLoginChallengeUsecase.Execute(c.Request().Context(), &usecase.ResolveLoginChallengeInput{
+	input := usecase.ResolveLoginChallengeInput{
 		LoginChallenge: c.QueryParam("login_challenge"),
-	})
+	}
+	output, err := s.resolveLoginChallengeUsecase.Execute(c.Request().Context(), &input)
 	if err != nil {
-		zerolog.Ctx(c.Request().Context()).Error().Err(err).Msg("failed to execute login usecase")
-
 		if errors.Is(err, usecase.ErrInvalidInput) {
-			var objectNotFoundErr *usecase.ObjectNotFoundErr
-			if errors.As(err, &objectNotFoundErr) && objectNotFoundErr.Object == "client" {
-				return s.fallbackToErrorPage(c, ErrCodeInvalidClient)
-			}
-
-			var objectInvalidErr *usecase.ObjectInvalidErr
-			if errors.As(err, &objectInvalidErr) && objectInvalidErr.Object == "bot" && objectInvalidErr.Field == "token" {
-				return s.fallbackToErrorPage(c, ErrCodeInvalidBotCredentials)
-			}
-
 			return s.fallbackToErrorPage(c, ErrCodeInvalidRequest)
 		}
 
 		return s.fallbackToErrorPage(c, ErrCodeInternalError)
 	}
 
-	if output == nil {
-		return s.fallbackToErrorPage(c, ErrCodeInternalError)
-	}
-
-	if output.Action == usecase.ResolveLoginChallengeActionRedirect {
-		if output.RedirectUri == nil {
-			return s.fallbackToErrorPage(c, ErrCodeInternalError)
-		}
+	switch output.Action {
+	case usecase.ResolveLoginChallengeActionRedirect:
 		return c.Redirect(http.StatusFound, *output.RedirectUri)
-	}
-
-	if output.WidgetUri == nil || output.MiniAppCallbackUri == nil {
+	case usecase.ResolveLoginChallengeActionRender:
+		return c.Render(http.StatusOK, "login", map[string]any{
+			"WidgetUri":          *output.WidgetUri,
+			"MiniAppCallbackUri": *output.MiniAppCallbackUri,
+		})
+	default:
 		return s.fallbackToErrorPage(c, ErrCodeInternalError)
 	}
-
-	return c.Render(http.StatusOK, "login", map[string]any{
-		"WidgetUri":          *output.WidgetUri,
-		"MiniAppCallbackUri": *output.MiniAppCallbackUri,
-	})
 }
